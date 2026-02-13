b93403a481017476ebbb3bda5f50b854041eaf8b
kennguyen291 <khangnguyen291@gmail.com>
2026-02-13 12:17:52 +1100

    Configure serverExternalPackages for LibSQL and update Prisma client initialization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  serverExternalPackages: ["@libsql/client", "@prisma/adapter-libsql"],
   /* config options here */
   allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
 };
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from "../generated/client/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
-
+import { createClient } from "@libsql/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaLibSql({
+const libsql = createClient({
     url: process.env.DATABASE_URL || "file:./dev.db",
+    authToken: process.env.TURSO_AUTH_TOKEN,
 });
+
+const adapter = new PrismaLibSql(libsql);
 
 export const prisma =
     globalForPrisma.prisma ||
